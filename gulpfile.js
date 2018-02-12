646d23b24d0cf21f76fe6fd7d8bc9e4a18bc0e03
2no553 <ninomiya.ryota@gmail.com>
2018-02-12 23:50:45 +0900

    midle save

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,10 @@ require plugin
 var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     notify = require('gulp-notify'),
+    browserSync = require('browser-sync').create(),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    cleanCSS = require('gulp-clean-css'),
-    browserSync = require('browser-sync').create();
+    cleanCSS = require('gulp-clean-css');
 /*
 
 =====================================
@@ -17,7 +17,7 @@ add folder
 =====================================
 */
 //folder to develop
-var develop = "public-html/";
+var develop = 'public-html/';
 //config path from gulpfile.js
 var config = {
 	'path' : {
@@ -32,37 +32,35 @@ glup task
 =====================================
 */
 //default
-gulp.task('default',function(){
-  // watch
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"+develop
+        }
+    });
+});
+
+gulp.task('watch',function(){
   gulp.watch(config.path.beforeCompileSass, ['sass']);
   gulp.watch(develop+'/*.html').on('change', browserSync.reload);
 
-  // Static server
-  browserSync.init({
-      server: {
-          baseDir: "./"+develop
-      }
-  });
-
-  //sass compile
-  gulp.task('sass', function(){
-    gulp.src(config.path.beforeCompileSass)
-      .pipe(plumber({
-        errorHandler: notify.onError('Error: <%= error.message %>')
-      }))
-      .pipe(sass())
-      .pipe(autoprefixer({
-          browsers: ['last 2 versions', 'ie >= 9', 'Android >= 4', 'ios_saf >= 8'],
-          cascade: false
-      }))
-      .pipe(cleanCSS())
-      .pipe(gulp.dest(config.path.afterCompileSass))
-      .pipe(browserSync.stream())
-  });
-
-  // reloading browsers
-  gulp.task('reload',function(){
-    browserSync.reload();
-  })
-
 });
+
+//sass compile
+gulp.task('sass', function(){
+  gulp.src(config.path.beforeCompileSass)
+    .pipe(plumber({
+      errorHandler: notify.onError('Error: <%= error.message %>')
+    }))
+    .pipe(sass())
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions', 'ie >= 9', 'Android >= 4', 'ios_saf >= 8'],
+        cascade: false
+    }))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(config.path.afterCompileSass))
+    .pipe(browserSync.stream())
+});
+
+// default task
+gulp.task('default', ['browser-sync', 'watch', 'sass']);
