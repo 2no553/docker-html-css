fb12c6b66d221e579a27601c9a566bddf9b4dd60
2no553 <ninomiya.ryota@gmail.com>
2018-02-13 03:30:26 +0900

    until ejs

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,15 @@ require plugin
 =====================================
 */
 var gulp = require('gulp'),
-    plumber = require('gulp-plumber'),
-    notify = require('gulp-notify'),
-    browserSync = require('browser-sync').create(),
-
-    newer = require('gulp-newer'),
-    imagemin = require('gulp-imagemin'),
-
-    uglify = require('gulp-uglify'),
-
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    cleanCSS = require('gulp-clean-css');
+    $ = require("gulp-load-plugins")(),
+    browserSync = require('browser-sync').create();
 
 /*
-
 =====================================
 add folder
 =====================================
 */
-//folder to develop -> srcに変更
+//folder to develop
 var develop = './public-html/';
 
 /*
@@ -31,7 +20,6 @@ var develop = './public-html/';
 glup task
 =====================================
 */
-//default
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
@@ -41,46 +29,49 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch',function(){
-  gulp.watch(develop+'sass/**/*.scss', ['sass']);
-  gulp.watch(develop+'/*.html').on('change', browserSync.reload);
+  gulp.watch(develop+'src/sass/**/*.scss', ['sass']);
+  gulp.watch(develop+'src/js/**/*.js', ['js']);
+  gulp.watch(develop+'*.html').on('change', browserSync.reload);
 });
 
-// image processing
 gulp.task('images', function() {
-  gulp.src(develop+'images/**/*')
-    .pipe(plumber({
-      errorHandler: notify.onError('Error: <%= error.message %>')
+  gulp.src(develop+'src/images/**/*')
+    .pipe($.plumber({
+      errorHandler: $.notify.onError('Error: <%= error.message %>')
     }))
-    .pipe(newer(develop+'test/images/'))
-    .pipe(imagemin({ optimizationLevel: 5 }))
-    .pipe(gulp.dest(develop+'test/images/'))
+    .pipe($.changed(develop+'images/'))
+    .pipe($.imagemin({ optimizationLevel: 5 }))
+    .pipe(gulp.dest(develop+'images/'))
 });
 
-// js processing
 gulp.task('js', function() {
-  gulp.src(develop+'js/**/*.js')
-    .pipe(plumber({
-      errorHandler: notify.onError('Error: <%= error.message %>')
+  gulp.src(develop+'src/js/**/*.js')
+    .pipe($.plumber({
+      errorHandler: $.notify.onError('Error: <%= error.message %>')
     }))
-    .pipe(uglify())
-    .pipe(gulp.dest(develop+'test/js/'))
+    .pipe($.uglify())
+    .pipe(gulp.dest(develop+'js/'))
 });
 
-//sass compile
-gulp.task('sass', function(){
-  gulp.src(develop+'sass/**/*.scss')
-    .pipe(plumber({
-      errorHandler: notify.onError('Error: <%= error.message %>')
+gulp.task('sass', ['images'], function(){
+  gulp.src(develop+'src/sass/**/*.scss')
+    .pipe($.plumber({
+      errorHandler: $.notify.onError('Error: <%= error.message %>')
     }))
-    .pipe(sass())
-    .pipe(autoprefixer({
+    .pipe($.sass())
+    .pipe($.autoprefixer({
         browsers: ['last 2 versions', 'ie >= 9', 'Android >= 4', 'ios_saf >= 8'],
         cascade: false
     }))
-    .pipe(cleanCSS())
+    .pipe($.csscomb())
+    .pipe($.groupCssMediaQueries())
+    .pipe($.cleanCss())
     .pipe(gulp.dest(develop+'css/'))
     .pipe(browserSync.stream())
 });
 
+// run all tasks
+gulp.task('run', ['js', 'sass']);
+
 // default task
-gulp.task('default', ['browser-sync', 'watch', 'sass', 'images', 'js']);
+gulp.task('default', ['browser-sync', 'watch', 'run']);
