69655e05282e569fa296b55843513d1a04519aaf
2no553 <ninomiya.ryota@gmail.com>
2018-02-12 13:32:15 +0900

    add gulp-

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,44 @@
-var gulp = require('gulp');
- 
-gulp.task('hello', function() {
-  console.log('Hello gulp');
+/*
+=====================================
+使用プラグイン（モジュールのロード）
+=====================================
+*/
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    notify = require('gulp-notify'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
+/*
+
+
+=====================================
+各ファイルのフォルダ指定
+=====================================
+*/
+//Folder to develop -> 開発フォルダ
+var develop = "public-html/";
+
+
+/*
+=====================================
+glup task
+=====================================
+*/
+
+//sassコンパイル
+gulp.task('sass', function(){
+  gulp.src(develop+'/sass/**/*.scss')
+    .pipe(plumber({
+      errorHandler: notify.onError('Error: <%= error.message %>')
+    }))
+    .pipe(sass())
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions', 'ie >= 9', 'Android >= 4', 'ios_saf >= 8'],
+        cascade: false
+    }))
+    .pipe(gulp.dest(develop+'/css'))
 });
- 
-gulp.task('default', ['hello']);
+
+
+//glup実行
+gulp.task('default', [ 'sass' ]);
