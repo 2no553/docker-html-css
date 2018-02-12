df4022ac4e70b052e8f2ac0394a7f3279e3cde85
2no553 <ninomiya.ryota@gmail.com>
2018-02-12 15:54:09 +0900

    add pluguin

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 /*
 =====================================
-使用プラグイン（モジュールのロード）
+require plugin
 =====================================
 */
 var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     notify = require('gulp-notify'),
     sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer');
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create();
 /*
 
-
 =====================================
-各ファイルのフォルダ指定
+add folder
 =====================================
 */
-//Folder to develop -> 開発フォルダ
+//folder to develop
 var develop = "public-html/";
 
 
@@ -24,21 +24,37 @@ var develop = "public-html/";
 glup task
 =====================================
 */
+//default
+gulp.task('default',function(){
+  // watch
+  gulp.watch(develop+'/sass/**/*.scss', ['sass']);
+  gulp.watch(develop+"/*.html", ['reload']);
 
-//sassコンパイル
-gulp.task('sass', function(){
-  gulp.src(develop+'/sass/**/*.scss')
-    .pipe(plumber({
-      errorHandler: notify.onError('Error: <%= error.message %>')
-    }))
-    .pipe(sass())
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions', 'ie >= 9', 'Android >= 4', 'ios_saf >= 8'],
-        cascade: false
-    }))
-    .pipe(gulp.dest(develop+'/css'))
+  // Static server
+  browserSync.init({
+      server: {
+          baseDir: "./"+develop
+      }
+  });
+
+  //sass compile
+  gulp.task('sass', function(){
+    gulp.src(develop+'/sass/**/*.scss')
+      .pipe(plumber({
+        errorHandler: notify.onError('Error: <%= error.message %>')
+      }))
+      .pipe(sass())
+      .pipe(autoprefixer({
+          browsers: ['last 2 versions', 'ie >= 9', 'Android >= 4', 'ios_saf >= 8'],
+          cascade: false
+      }))
+      .pipe(gulp.dest(develop+'/css'))
+      .pipe(browserSync.stream())
+  });
+
+  // reloading browsers
+  gulp.task('reload',function(){
+    browserSync.reload();
+  })
+
 });
-
-
-//glup実行
-gulp.task('default', [ 'sass' ]);
